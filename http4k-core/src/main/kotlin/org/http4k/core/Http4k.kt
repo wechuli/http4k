b4383367cc7c203243f6abe1911478d636202f02
David Denton <denton.david@gmail.com>
2018-12-15 15:11:43 +0000

    start again with coroutine version

--- http4k-core/src/main/kotlin/org/http4k/core/Http4k.kt
+++ http4k-core/src/main/kotlin/org/http4k/core/Http4k.kt
@@ -2,20 +2,33 @@ package org.http4k.core
 
 import org.http4k.routing.RoutingHttpHandler
 
-typealias HttpHandler = (Request) -> Response
+interface HttpHandler {
+    suspend operator fun invoke(request: Request): Response
 
-interface Filter : (HttpHandler) -> HttpHandler {
     companion object {
-        operator fun invoke(fn: (HttpHandler) -> HttpHandler): Filter = object : Filter {
-            override operator fun invoke(next: HttpHandler): HttpHandler = fn(next)
+        operator fun invoke(fn: suspend (Request) -> Response): HttpHandler = object : HttpHandler {
+            override suspend operator fun invoke(request: Request): Response = fn(request)
+        }
+    }
+
+}
+
+interface Filter {
+    suspend operator fun invoke(next: HttpHandler): HttpHandler
+
+    companion object {
+        operator fun invoke(fn: suspend (HttpHandler) -> HttpHandler): Filter = object : Filter {
+            override suspend operator fun invoke(next: HttpHandler): HttpHandler = fn(next)
         }
     }
 }
 
-val Filter.Companion.NoOp: Filter get() = Filter { next -> { next(it) } }
+val Filter.Companion.NoOp: Filter get() = Filter { next -> HttpHandler { next(it) } }
+
+fun Filter.then(fn: suspend (Request) -> Response) = then(HttpHandler(fn))
 
 fun Filter.then(next: Filter): Filter = Filter { this(next(it)) }
 
-fun Filter.then(next: HttpHandler): HttpHandler = { this(next)(it) }
+fun Filter.then(next: HttpHandler): HttpHandler = HttpHandler { this(next)(it) }
 
 fun Filter.then(routingHttpHandler: RoutingHttpHandler): RoutingHttpHandler = routingHttpHandler.withFilter(this)
