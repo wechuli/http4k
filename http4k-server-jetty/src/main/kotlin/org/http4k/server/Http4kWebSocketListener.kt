569c41fefec6773e28a89fdb7d34d6a605d17bae
David Denton <denton.david@gmail.com>
2019-01-24 12:16:36 +0000

    websocket creatoruse blocking on websocket connection

--- http4k-server-jetty/src/main/kotlin/org/http4k/server/Http4kWebSocketListener.kt
+++ http4k-server-jetty/src/main/kotlin/org/http4k/server/Http4kWebSocketListener.kt
@@ -2,6 +2,7 @@ package org.http4k.server
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.eclipse.jetty.websocket.api.Session
 import org.eclipse.jetty.websocket.api.WebSocketListener
 import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest
@@ -20,14 +21,14 @@ import java.nio.ByteBuffer
 internal class Http4kWebSocketAdapter internal constructor(private val innerSocket: PushPullAdaptingWebSocket) {
     suspend fun onError(throwable: Throwable) = innerSocket.triggerError(throwable)
     suspend fun onClose(statusCode: Int, reason: String?) = innerSocket.triggerClose(WsStatus(statusCode, reason
-            ?: "<unknown>"))
+        ?: "<unknown>"))
 
     suspend fun onMessage(body: Body) = innerSocket.triggerMessage(WsMessage(body))
 }
 
 internal fun ServletUpgradeRequest.asHttp4kRequest(): Request =
-        Request(Method.valueOf(method), Uri.of(requestURI.toString()))
-                .headers(headerParameters())
+    Request(Method.valueOf(method), Uri.of(requestURI.toString()))
+        .headers(headerParameters())
 
 private fun ServletUpgradeRequest.headerParameters(): Headers = headers.asSequence().fold(listOf()) { memo, next -> memo + next.value.map { next.key to it } }
 
@@ -41,20 +42,20 @@ internal class Http4kWebSocketListener(private val wSocket: WsConsumer, private 
     }
 
     override fun onWebSocketConnect(session: Session) {
-        scope.launch {
+        runBlocking {
             websocket = Http4kWebSocketAdapter(
-                    object : PushPullAdaptingWebSocket(upgradeRequest) {
-                        override suspend fun send(message: WsMessage) {
-                            when (message.body) {
-                                is StreamBody -> session.remote.sendBytes(message.body.payload)
-                                else -> session.remote.sendString(message.bodyString())
-                            }
+                object : PushPullAdaptingWebSocket(upgradeRequest) {
+                    override suspend fun send(message: WsMessage) {
+                        when (message.body) {
+                            is StreamBody -> session.remote.sendBytes(message.body.payload)
+                            else -> session.remote.sendString(message.bodyString())
                         }
+                    }
 
-                        override suspend fun close(status: WsStatus) {
-                            session.close(status.code, status.description)
-                        }
-                    }.apply { wSocket(this) }
+                    override suspend fun close(status: WsStatus) {
+                        session.close(status.code, status.description)
+                    }
+                }.apply { wSocket(this) }
             )
         }
     }
