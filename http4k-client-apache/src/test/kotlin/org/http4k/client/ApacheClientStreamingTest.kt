3503dbd1daf3c1b08ccd9174531c86529c10565e
Ivan Sanchez <ivan@gourame.com>
2019-02-05 23:56:40 -0200

    Replace apache httpclient with httpasyncclient + coroutines in http-client-apache

--- http4k-client-apache/src/test/kotlin/org/http4k/client/ApacheClientStreamingTest.kt
+++ http4k-client-apache/src/test/kotlin/org/http4k/client/ApacheClientStreamingTest.kt
@@ -1,18 +1,17 @@
 package org.http4k.client
 
-import org.apache.http.config.SocketConfig
-import org.apache.http.impl.client.HttpClients
+import org.apache.http.impl.nio.client.HttpAsyncClients
+import org.apache.http.impl.nio.reactor.IOReactorConfig
 import org.http4k.core.BodyMode.Stream
 import org.http4k.server.SunHttp
 
 class ApacheClientStreamingTest : HttpClientContract({ SunHttp(it) },
     ApacheClient(requestBodyMode = Stream, responseBodyMode = Stream),
-    ApacheClient(HttpClients.custom()
-        .setDefaultSocketConfig(
-            SocketConfig.custom()
-                .setSoTimeout(100)
-                .build()
-        ).build(),
+    ApacheClient(HttpAsyncClients.custom()
+        .setDefaultIOReactorConfig(IOReactorConfig.custom()
+            .setSoTimeout(100)
+            .build()).build().apply { start() }
+        ,
         responseBodyMode = Stream,
         requestBodyMode = Stream)
 )
--- http4k-client-apache/src/test/kotlin/org/http4k/client/ApacheClientTest.kt
+++ http4k-client-apache/src/test/kotlin/org/http4k/client/ApacheClientTest.kt
@@ -2,13 +2,13 @@ package org.http4k.client
 
 import com.natpryce.hamkrest.assertion.assertThat
 import kotlinx.coroutines.runBlocking
-import org.apache.http.HttpHost
-import org.apache.http.HttpRequest
-import org.apache.http.client.methods.CloseableHttpResponse
-import org.apache.http.config.SocketConfig
+import org.apache.http.concurrent.FutureCallback
 import org.apache.http.conn.ConnectTimeoutException
-import org.apache.http.impl.client.CloseableHttpClient
-import org.apache.http.impl.client.HttpClients
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient
+import org.apache.http.impl.nio.client.HttpAsyncClients
+import org.apache.http.impl.nio.reactor.IOReactorConfig
+import org.apache.http.nio.protocol.HttpAsyncRequestProducer
+import org.apache.http.nio.protocol.HttpAsyncResponseConsumer
 import org.apache.http.protocol.HttpContext
 import org.http4k.core.BodyMode.Stream
 import org.http4k.core.Method.GET
@@ -17,25 +17,27 @@ import org.http4k.core.Status.Companion.CLIENT_TIMEOUT
 import org.http4k.hamkrest.hasStatus
 import org.http4k.server.SunHttp
 import org.junit.jupiter.api.Test
+import java.util.concurrent.Future
+import java.util.concurrent.FutureTask
 
 class ApacheClientTest : HttpClientContract({ SunHttp(it) }, ApacheClient(),
-    ApacheClient(HttpClients.custom()
-        .setDefaultSocketConfig(
-            SocketConfig.custom()
-                .setSoTimeout(100)
-                .build()
-        ).build()
+    ApacheClient(HttpAsyncClients.custom()
+        .setDefaultIOReactorConfig(IOReactorConfig.custom()
+            .setSoTimeout(100)
+            .build()).build().apply { start() }
         , responseBodyMode = Stream)) {
 
     @Test
     fun `connect timeout is handled`() = runBlocking {
-        assertThat(ApacheClient(object : CloseableHttpClient() {
-            override fun getParams() = TODO("not implemented")
+        assertThat(ApacheClient(object : CloseableHttpAsyncClient() {
+            override fun isRunning(): Boolean = true
 
-            override fun getConnectionManager() = TODO("not implemented")
+            override fun start() = Unit
 
-            override fun doExecute(target: HttpHost?, request: HttpRequest?, context: HttpContext?): CloseableHttpResponse {
-                throw ConnectTimeoutException()
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : Any> execute(requestProducer: HttpAsyncRequestProducer, responseConsumer: HttpAsyncResponseConsumer<T>, context: HttpContext, callback: FutureCallback<T>): Future<T> {
+                callback.failed(ConnectTimeoutException())
+                return FutureTask<T> { error("task should have finished with ConnectionTimeoutException") }
             }
 
             override fun close() {
