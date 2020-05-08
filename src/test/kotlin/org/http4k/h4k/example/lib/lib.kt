0552c2add2293cace637e3780cb2e14ef243420d
David Denton <denton.david@gmail.com>
2020-05-08 21:40:09 +0100

    externalise sideeffect

--- src/test/kotlin/org/http4k/h4k/example/lib/lib.kt
+++ src/test/kotlin/org/http4k/h4k/example/lib/lib.kt
@@ -7,21 +7,30 @@ import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
 interface Discovery<ServiceId> {
+    /**
+     * Lookup a Service HttpHandler by it's identifier
+     */
     fun lookup(id: ServiceId): HttpHandler
 }
 
-class H4KCluster<ServiceId> : Discovery<ServiceId> {
+class H4KCluster<ServiceId>(private val log: (String) -> Unit = ::println) : Discovery<ServiceId> {
     private val services = mutableMapOf<ServiceId, HttpHandler>()
     private val servers = mutableListOf<Pair<ServiceId, Http4kServer>>()
 
     override fun lookup(id: ServiceId) = services[id]
         ?: throw IllegalStateException("$id is not registered in this cluster")
 
+    /**
+     * Install a Service HttpHandler into the cluster
+     */
     fun install(id: ServiceId, appFn: (Discovery<ServiceId>) -> HttpHandler) = apply {
         val app = appFn(this)
         services[id] = app
     }
 
+    /**
+     * Bind a Service HttpHandler to an HTTP port
+     */
     fun expose(id: ServiceId, port: Port) = apply {
         servers += id to lookup(id).asServer(SunHttp(port.value))
     }
@@ -29,14 +38,14 @@ class H4KCluster<ServiceId> : Discovery<ServiceId> {
     fun start() = apply {
         servers.forEach {
             it.second.start()
-            println("Bound ${it.first} to ${it.second.port()}")
+            log("Bound ${it.first} to ${it.second.port()}")
         }
     }
 
     fun stop() = apply {
         servers.forEach {
             it.second.stop()
-            println("Unbound ${it.first}")
+            log("Unbound ${it.first}")
         }
     }
 }
