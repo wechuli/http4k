d25412a3aaae56f5ce7025fe6cecc29985cd2479
David Denton <denton.david@gmail.com>
2020-05-08 19:49:08 +0100

    more registry work..

--- src/test/kotlin/org/http4k/h4k/lib.kt
+++ src/test/kotlin/org/http4k/h4k/lib.kt
@@ -25,18 +25,19 @@ interface Registry<ServiceId> : Discovery<ServiceId> {
 }
 
 /**
- * Use this when running in deployed K8S cluster to register and lookup K8S services
+ * Use this when running an app in a deployed K8S cluster to register and lookup other K8S services
  */
 class K8SServiceRegistry<ServiceId>(private val port: (ServiceId) -> Port = { Port(8080) }) : Registry<ServiceId> {
 
-    private val services = mutableMapOf<ServiceId, Http4kServer>()
+    private var server: Http4kServer? = null
 
     override fun register(id: ServiceId, http: HttpHandler): ServerControl = {
-        services[id] = it().start()
+        if (server != null) throw IllegalStateException("There is already a server started in this pod!")
+        server = it().start()
     }
 
     override fun unregister(id: ServiceId) {
-        services[id]?.stop()
+        server?.stop()
     }
 
     override fun lookup(id: ServiceId) = ClientFilters.SetHostFrom(Uri.of("http://${id}:${port(id).value}")).then(OkHttp())
@@ -88,19 +89,21 @@ class H4KRegistry<ServiceId> : Registry<ServiceId> {
 }
 
 class RegisteringServerConfig<ServiceId>(
-    private val name: ServiceId,
+    private val id: ServiceId,
     private val registry: Registry<ServiceId>,
     private val serverConfig: () -> ServerConfig) : ServerConfig {
 
     override fun toServer(httpHandler: HttpHandler) = object : Http4kServer {
+        private val server by lazy { serverConfig().toServer(httpHandler) }
+
         override fun start() = apply {
-            registry.register(name, httpHandler).invoke { serverConfig().toServer(httpHandler) }
+            registry.register(id, httpHandler).invoke { server }
         }
 
         override fun stop() = apply {
-            registry.unregister(name)
+            registry.unregister(id)
         }
 
-        override fun port() = serverConfig().toServer(httpHandler).port()
+        override fun port() = server.port()
     }
 }
