308cbd6ce8a8774882612a4ca91138962495fa88
David Denton <denton.david@gmail.com>
2020-08-21 20:19:20 +0100

    playing with clusters

--- src/test/kotlin/org/http4k/h4k/example/test/external/reverser/FakeReverser.kt
+++ src/test/kotlin/org/http4k/h4k/example/test/external/reverser/FakeReverser.kt
@@ -3,8 +3,7 @@ package org.http4k.h4k.example.test.external.reverser
 import org.http4k.core.HttpHandler
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
-import org.http4k.h4k.example.main.external.doubler.Doubler
-import org.http4k.h4k.example.main.external.doubler.ID
+import org.http4k.h4k.example.main.external.reverser.ID
 import org.http4k.h4k.example.main.external.reverser.Reverser
 import org.http4k.h4k.example.main.internal.RunningServerInfra
 import org.http4k.h4k.example.main.internal.asAppServer
@@ -17,5 +16,5 @@ fun Reverser.Companion.App(reverser: Reverser): HttpHandler = {
     Response(OK).body(reverser(it.bodyString())) }
 
 fun main() {
-    RunningServerInfra(Doubler.ID).asAppServer { Reverser.App(Reverser.Domain()) }.start()
+    RunningServerInfra(Reverser.ID).asAppServer { Reverser.App(Reverser.Domain()) }.start()
 }
